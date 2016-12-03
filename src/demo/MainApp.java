072a3781ef2b0eb3b60d6aae5ad052a6e4a716d2
Viet <vietsemantics@gmail.com>
2016-12-02 20:42:06 -0600

    add code

--- src/demo/MainApp.java
+++ src/demo/MainApp.java
@@ -3,17 +3,19 @@ package demo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import classes.HelloSpring;
+
 public class MainApp {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("demo/Beans.xml");
 
-		HelloSpring obj = (HelloSpring) context.getBean("helloWorldx");
+		HelloSpring obj = (HelloSpring) context.getBean("HelloSpring");
 
 		
 		obj.setMessage(" I'm a new message");
-		obj.getMessage();
+		obj.showMessage();
 		
-		HelloSpring obj2 = (HelloSpring) context.getBean("helloWorldx");
-		obj2.getMessage();
+		HelloSpring obj2 = (HelloSpring) context.getBean("HelloSpring");
+		obj2.showMessage();
 	}
 }
